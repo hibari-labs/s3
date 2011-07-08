b13b879c1bff8a7f6d38b14f842ab7d2386e861b
Satoshi Kinoshita <kino@geminimobile.com>
2011-07-08 16:25:31 +0900

    eunit for client

---
+++ test/eunit/s3_client_tests.erl
@@ -0,0 +1,33 @@
+%%%----------------------------------------------------------------------
+%%% Copyright (c) 2008-2011 Gemini Mobile Technologies, Inc.  All rights reserved.
+%%%
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%
+%%% File    : gmt_time_tests.erl
+%%% Purpose : GMT time test suite
+%%%----------------------------------------------------------------------
+
+-module(s3_client_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(MUT, s3_client). % Module Under Test (a.k.a. DUT)
+-define(ATM, ?MODULE). % Automatic Test Module (a.k.a. ATE)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% Test Cases
+%%
+
+start_1_000_test() ->
+    ok.
